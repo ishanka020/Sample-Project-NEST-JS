e5b0dc1fa9e5041f43edb4c19ef842a5a6e7de95
Ishanka Perera <pererawcpi.20@itfac.mrt.ac.lk>
2025-10-03 13:31:09 +0530

    DTO Schemas, Validation and Pipes

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  isNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsEnum(['ADMIN', 'ENGINEER', 'INTERN'], {
+    message: 'Valid role required',
+  })
+  role: 'ADMIN' | 'ENGINEER' | 'INTERN';
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUsetDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   private users = [
@@ -37,42 +39,36 @@ export class UsersService {
 
   findAll(role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
     if (role) {
-      return this.users.filter((user) => user.role === role);
+      const rolesArray = this.users.filter((user) => user.role === role);
+      if (rolesArray.length === 0)
+        throw new NotFoundException('User Role Not Found');
+      return rolesArray;
     }
     return this.users;
   }
 
   findOne(id: number) {
     const user = this.users.find((user) => user.id === id);
+
+    if (!user) throw new NotFoundException('User Not Found');
     return user;
   }
 
-  create(user: {
-    name: string;
-    email: string;
-    role: 'INTERN' | 'ADMIN' | 'ENGINEER';
-  }) {
+  create(createUserDto: CreateUserDto) {
     const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
     const newUser = {
       id: usersByHighestId[0].id + 1,
-      ...user,
+      ...createUserDto,
     };
 
     this.users.push(newUser);
     return newUser;
   }
 
-  update(
-    id: number,
-    updatedUser: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ADMIN' | 'ENGINEER';
-    },
-  ) {
+  update(id: number, updateUserDto: UpdateUsetDto) {
     this.users = this.users.map((user) => {
       if (user.id === id) {
-        return { ...user, ...updatedUser };
+        return { ...user, ...updateUserDto };
       }
       return user;
     });
