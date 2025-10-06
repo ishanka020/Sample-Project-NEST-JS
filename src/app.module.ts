b1ece3698b47201f25820fe02dcce77e57eb1bc7
Ishanka Perera <pererawcpi.20@itfac.mrt.ac.lk>
2025-10-06 12:11:00 +0530

    Server logs and Exception Filters added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,28 @@ import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule, ThrottlerModule.forRoot([{ttl: 60000, limit: 3,}])],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+    MyLoggerModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, {provide: APP_GUARD, useClass:ThrottlerGuard }],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
